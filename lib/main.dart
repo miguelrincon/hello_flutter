68cb929b2bb5d4c080fc1096eb0197f3cd2de4e7
Miguel Rincon <miguel.rincon86@gmail.com>
2019-03-04 11:10:18 -0500

    A few style fixes

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'I Generatez Names',
       theme: new ThemeData(
         // Add the 3 lines from here...
         primaryColor: Colors.white,
